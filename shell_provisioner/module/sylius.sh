abf8f30347d5eb178091ebcbc8d053d0260b7880
Usama Ahmed <maxpaynestory@gmail.com>
2017-05-19 00:08:49 +0200

    used default password of mysql
    Mysql login root without password is used instead of vagrant.

--- shell_provisioner/module/sylius.sh
+++ shell_provisioner/module/sylius.sh
@@ -6,7 +6,7 @@ composer create-project -s beta -n sylius/sylius-standard ./sylius
 
 cd sylius
 
-sed -i "s/database_password: null/database_password: vagrant/g" app/config/parameters.yml
+#sed -i "s/database_password: null/database_password: vagrant/g" app/config/parameters.yml
 
 php bin/console sylius:install --no-interaction
 php bin/console sylius:fixtures:load
