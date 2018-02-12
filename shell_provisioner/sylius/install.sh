43368ef33151fb014cbcd4a6e0764783e5b5fbb6
Michał Żelazkiewicz <michal.zelazkiewicz@gmail.com>
2018-02-12 18:24:40 +0100

    Fix problem with null password during vagrant up

--- shell_provisioner/sylius/install.sh
+++ shell_provisioner/sylius/install.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/sites/sylius
 
-sed -i "s/database_password: null/database_password: vagrant/g" app/config/parameters.yml
+sed -i "s/env(SYLIUS_DATABASE_PASSWORD): null/env(SYLIUS_DATABASE_PASSWORD): vagrant/g" app/config/parameters.yml
 
 php bin/console sylius:install --no-interaction
 php bin/console sylius:fixtures:load
